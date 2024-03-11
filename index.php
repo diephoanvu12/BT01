745dc92a95cc2c2e3c9961eceeac73761e69bc9f
Eric <ethancurtis371998@gmail.com>
2024-03-11 14:08:22 +0700

    username

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<h1>Hello<h1>    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="col-md-6 col-sm-6">
+        <div class="customer-login-register">
+            <div class="form-login-title">
+                <h2>Đăng nhập tài khoản</h2>
+            </div>
+            <div class="login-form">
+                <form id="form_reg" action="login.php" method="post">
+                    <div class="form-fild">
+                        <p><label>Tên đăng nhập<span class="required">*</span></label></p>
+                        <input type="text" name="username" class="form-control" id="username">
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
